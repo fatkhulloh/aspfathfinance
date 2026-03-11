5c94054ac59bab0e8f4314ffa99962256ebf888a
Fatkhulloh Hol <33220438+fatkhulloh@users.noreply.github.com>
2026-03-11 10:54:07 +0700

    Fix navbar

---
+++ ImsMVC/Controllers/FinanceController.cs
@@ -0,0 +1,62 @@
+﻿using ImsMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImsMVC.Controllers
+{
+    public class FinanceController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Hitung(LoanModel model)
+        {
+            model.DP = model.OTR * 0.20m;
+            model.PokokUtang = model.OTR - model.DP;
+
+            if (model.Tenor <= 12)
+                model.Bunga = 0.12;
+            else if (model.Tenor <= 24)
+                model.Bunga = 0.14;
+            else
+                model.Bunga = 0.165;
+
+            decimal bungaTotal = model.PokokUtang * (decimal)model.Bunga;
+
+            model.TotalHutang = model.PokokUtang + bungaTotal;
+
+            model.AngsuranPerBulan = model.TotalHutang / model.Tenor;
+
+            return View("Result", model);
+        }
+
+
+        public ActionResult Jadwal(LoanModel model)
+        {
+            List<dynamic> jadwal = new List<dynamic>();
+
+            DateTime tanggal = model.StartDate;
+
+            for (int i = 1; i <= model.Tenor; i++)
+            {
+                jadwal.Add(new
+                {
+                    AngsuranKe = i,
+                    Angsuran = model.AngsuranPerBulan,
+                    JatuhTempo = tanggal.AddMonths(i)
+                });
+            }
+
+            ViewBag.Jadwal = jadwal;
+
+            return View(model);
+        }
+
+    }
+}
---
+++ ImsMVC/Models/LoanModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ImsMVC.Models
+{
+    public class LoanModel
+    {
+        public string KontrakNo { get; set; }
+        public string ClientName { get; set; }
+        public decimal OTR { get; set; }
+
+        public decimal DP { get; set; }
+        public decimal PokokUtang { get; set; }
+        public int Tenor { get; set; }
+        public double Bunga { get; set; }
+
+        public decimal TotalHutang { get; set; }
+        public decimal AngsuranPerBulan { get; set; }
+
+        public DateTime StartDate { get; set; }
+    }
+}
